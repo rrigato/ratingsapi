faa809bb23ecddf8b0694267ece867bb7a0768d2
Ryan <ryanrigato@gmail.com>
2020-07-25 10:10:40 -0500

    shows endpoint lambda function

---
+++ microservices/shows/shows.py
@@ -0,0 +1,93 @@
+import boto3
+import logging
+
+
+def get_boto_clients(resource_name, region_name="us-east-1",
+    table_name=None):
+    """Returns the boto client for various aws resources
+
+        Parameters
+        ----------
+        resource_name : str
+            Name of the resource for the client
+
+        region_name : str
+                aws region you are using, defaults to
+                us-east-1
+
+        Returns
+        -------
+        service_client : boto3.client
+            boto3 client for the aws resource in resource_name
+            in region region_name
+
+        dynamodb_table_resource : boto3.resource.Table
+            boto3 Table resource, only returned if table_name is
+            not None
+        
+
+
+        Raises
+        ------
+    """
+
+    service_client = boto3.client(
+            service_name=resource_name, 
+            region_name=region_name
+        )
+
+    '''
+        return boto3 DynamoDb table resource in addition to boto3 client
+        if table_name parameter is not None
+    '''
+    if table_name is not None:
+        dynamodb_table_resource = boto3.resource(
+            service_name=resource_name,
+            region_name=region_name
+        ).Table(table_name)
+
+        return(service_client, dynamodb_table_resource)
+
+    '''
+        Otherwise return just a resource client
+    '''
+    return(service_client)
+
+
+
+
+
+def main():
+    """Entry point into the script
+        Parameters
+        ----------
+
+        Returns
+        -------
+
+        Raises
+        ------
+    """
+    pass
+
+def lambda_handler(event, context):
+    """Handles lambda invocation from cloudwatch events rule
+
+        Parameters
+        ----------
+
+        Returns
+        -------
+
+        Raises
+        ------
+    """
+    '''
+        Logging required for cloudwatch logs
+    '''
+    logging.getLogger().setLevel(logging.INFO)
+    main()
+
+
+if __name__ == "__main__":   
+    main()
