54c1e7fa1b3abb2b0ae521b4ebb49d62b1696927
Ryan <ryanrigato@gmail.com>
2020-08-01 04:15:11 -0500

    descrfibe update

--- builds/iterate_lambda.sh
+++ builds/iterate_lambda.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
+cd microservices
+
+# iterate over all directories in microservices
 for lambda_code in */ ; do 
   if [[ -d "$lambda_code" && ! -L "$lambda_code" ]]; then
-          #shows/ to shows
+    #shows/ to shows
     endpoint_name="${lambda_code%/}"
     echo "$endpoint_name is a directory"; 
 
     cd $endpoint_name
-          #create deployment archive
+    #create deployment archive
     zip -r9 "${endpoint_name}.zip" .
 
-          #updates zip deployment package
+    #updates zip deployment package
     aws lambda update-function-code \
             --function-name "${PROJECT_NAME}-${endpoint_name}-endpoint-${ENVIRON_PREFIX}" \
             --zip-file "fileb://${endpoint_name}.zip"
 
-          #changes lambda handler to shows.lambda_handler
+    #changes lambda handler to shows.lambda_handler
     aws lambda update-function-configuration \
             --function-name "${PROJECT_NAME}-${endpoint_name}-endpoint-${ENVIRON_PREFIX}" \
             --handler "${endpoint_name}.lambda_handler" \
@@ -28,3 +31,5 @@ for lambda_code in */ ; do
     echo "${PROJECT_NAME}-${endpoint_name}-endpoint-${ENVIRON_PREFIX}"
   fi; 
 done
+
+cd ..
