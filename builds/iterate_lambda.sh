e3ba849d0182716f3d1c7ac6f92637f6816d629e
Ryan <ryanrigato@gmail.com>
2020-08-01 04:09:14 -0500

    adding separate bash script

---
+++ builds/iterate_lambda.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+for lambda_code in */ ; do 
+  if [[ -d "$lambda_code" && ! -L "$lambda_code" ]]; then
+          #shows/ to shows
+    endpoint_name="${lambda_code%/}"
+    echo "$endpoint_name is a directory"; 
+
+    cd $endpoint_name
+          #create deployment archive
+    zip -r9 "${endpoint_name}.zip" .
+
+          #updates zip deployment package
+    aws lambda update-function-code \
+            --function-name "${PROJECT_NAME}-${endpoint_name}-endpoint-${ENVIRON_PREFIX}" \
+            --zip-file "fileb://${endpoint_name}.zip"
+
+          #changes lambda handler to shows.lambda_handler
+    aws lambda update-function-configuration \
+            --function-name "${PROJECT_NAME}-${endpoint_name}-endpoint-${ENVIRON_PREFIX}" \
+            --handler "${endpoint_name}.lambda_handler" \
+            --runtime python3.8
+
+    mv "${endpoint_name}.zip" ..
+          
+    cd ..
+
+          #lambda function name
+    echo "${PROJECT_NAME}-${endpoint_name}-endpoint-${ENVIRON_PREFIX}"
+  fi; 
+done
